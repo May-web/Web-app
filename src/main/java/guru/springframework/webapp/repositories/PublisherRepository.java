518725729b46266d4c23a32ed129878c3b49e13b
May-web <57176447+May-web@users.noreply.github.com>
2021-04-04 21:02:53 +0200

    Initial commit

---
+++ src/main/java/guru/springframework/webapp/repositories/PublisherRepository.java
@@ -0,0 +1,9 @@
+package guru.springframework.webapp.repositories;
+
+import guru.springframework.webapp.domain.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+
+
+}
